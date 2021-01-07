f1b396193f86c4f1e56305e604e6d81e78389dc0
Chris <ckeenan@starry.com>
2021-01-07 18:25:41 +0700

    Bumped version to 0.2.0

--- buildSrc/src/main/java/com.chrynan.emoji.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.emoji.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.emoji"
     const val owner = "chrynan"
     const val repoName = "emoji"
-    const val versionName = "0.1.1"
-    const val versionCode = 2
-    const val versionDescription = "Pre-release 0.1.1"
+    const val versionName = "0.2.0"
+    const val versionCode = 3
+    const val versionDescription = "Pre-release 0.2.0"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/emoji.git"
     const val androidCompileSdkVersion = 29
