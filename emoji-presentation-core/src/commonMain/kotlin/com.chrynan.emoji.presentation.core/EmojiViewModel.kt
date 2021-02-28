89b9245c9861fdaca94215920ce135c8a809404a
Chris <ckeenan@starry.com>
2021-02-28 15:30:14 +0700

    Created Emoji.toViewModel function

--- emoji-presentation-core/src/commonMain/kotlin/com.chrynan.emoji.presentation.core/EmojiViewModel.kt
+++ emoji-presentation-core/src/commonMain/kotlin/com.chrynan.emoji.presentation.core/EmojiViewModel.kt
@@ -1,9 +1,26 @@
+@file:Suppress("unused")
+
 package com.chrynan.emoji.presentation.core
 
 import com.chrynan.emoji.core.Emoji
 
+/**
+ * A View Model representation of an [Emoji].
+ *
+ * @property [emoji] The [Emoji] this View Model represents.
+ * @property [isIconPreferred] A [Boolean] indicating whether the [Emoji.Unicode.iconUri] is preferred if it is
+ * available.
+ * @property [isStaticUriPreferred] A [Boolean] indicating whether the [Emoji.Custom.staticUri] is preferred if it is
+ * available.
+ */
 data class EmojiViewModel(
     val emoji: Emoji,
     val isIconPreferred: Boolean = false,
     val isStaticUriPreferred: Boolean = false
 )
+
+/**
+ * A convenience function for converting this [Emoji] into an [EmojiViewModel].
+ */
+fun Emoji.toViewModel(isIconPreferred: Boolean = false, isStaticUriPreferred: Boolean = false): EmojiViewModel =
+    EmojiViewModel(emoji = this, isIconPreferred = isIconPreferred, isStaticUriPreferred = isStaticUriPreferred)
