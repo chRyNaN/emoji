c276a5deac782789d88691090eef0b13ffddee20
Chris <ckeenan@starry.com>
2021-03-06 14:24:16 +0700

    Added support to retrieving emoji fonts from a File

--- emoji-presentation-android/src/main/java/com.chrynan.emoji.presentation.android/EmojiConfigs.kt
+++ emoji-presentation-android/src/main/java/com.chrynan.emoji.presentation.android/EmojiConfigs.kt
@@ -7,6 +7,8 @@ import androidx.core.provider.FontRequest
 import androidx.emoji.bundled.BundledEmojiCompatConfig
 import androidx.emoji.text.EmojiCompat
 import androidx.emoji.text.FontRequestEmojiCompatConfig
+import de.c1710.filemojicompat.FileEmojiCompatConfig
+import java.io.File
 
 /**
  * An object providing common [EmojiCompat.Config] instances for Android.
@@ -41,4 +43,31 @@ object EmojiConfigs {
         context: Context,
         fontRequest: FontRequest
     ): EmojiCompat.Config = FontRequestEmojiCompatConfig(context, fontRequest)
+
+    /**
+     * Retrieves an [EmojiCompat.Config] from the provided [fontFile] and [context].
+     *
+     * Note that if the [fontFile] is located in the device's storage, permissions are required to
+     * access it. If the permissions weren't added appropriately, this will result in an error,
+     * either in this function or when trying to use emojis.
+     */
+    fun getFromFile(
+        context: Context,
+        fontFile: File
+    ): EmojiCompat.Config = FileEmojiCompatConfig(context, fontFile)
+}
+
+/**
+ * A convenience function for calling [EmojiConfigs.getFromFile] by creating a [File] with the
+ * provided [fontFilePath].
+ *
+ * @see [EmojiConfigs.getFromFile]
+ */
+fun EmojiConfigs.getFromFile(
+    context: Context,
+    fontFilePath: String
+): EmojiCompat.Config {
+    val fontFile = File(fontFilePath)
+
+    return FileEmojiCompatConfig(context, fontFile)
 }
