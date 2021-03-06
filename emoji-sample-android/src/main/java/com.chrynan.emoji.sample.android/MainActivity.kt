02d45c12c0479d6ac0f89077ac3fcba9f4bca099
Chris <ckeenan@starry.com>
2021-03-06 16:12:18 +0700

    Added EmojiChip to Android Sample

--- emoji-sample-android/src/main/java/com.chrynan.emoji.sample.android/MainActivity.kt
+++ emoji-sample-android/src/main/java/com.chrynan.emoji.sample.android/MainActivity.kt
@@ -3,7 +3,9 @@ package com.chrynan.emoji.sample.android
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.chrynan.dispatchers.dispatchers
 import com.chrynan.emoji.presentation.android.util.showEmojiPicker
+import com.chrynan.emoji.presentation.android.widget.EmojiChip
 import com.chrynan.emoji.presentation.android.widget.EmojiWidget
 import com.chrynan.emoji.presentation.core.listener.EmojiListItemSelectedListener
 import com.chrynan.emoji.presentation.core.viewmodel.EmojiViewModel
@@ -11,6 +13,11 @@ import com.chrynan.emoji.repo.sqlite.EmojiDatabase
 import com.chrynan.emoji.repo.sqlite.SqliteEmojiRepository
 import com.chrynan.sample.R
 import com.squareup.sqldelight.android.AndroidSqliteDriver
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.chrynan.emoji.presentation.core.viewmodel.toViewModel
+import com.chrynan.emoji.core.getByNameOrNull
 
 class MainActivity : AppCompatActivity(),
     EmojiListItemSelectedListener {
@@ -29,9 +36,42 @@ class MainActivity : AppCompatActivity(),
         emojiDialogButton.setOnClickListener {
             supportFragmentManager.showEmojiPicker(repository = repository)
         }
+
+        setupChips()
     }
 
     override fun onEmojiListItemSelected(item: EmojiViewModel) {
         emojiWidget.emojiViewModel = item
+    }
+
+    private fun setupChips() {
+        val emojiChipOne = findViewById<EmojiChip>(R.id.emojiDialogChipOne)
+        val emojiChipTwo = findViewById<EmojiChip>(R.id.emojiDialogChipTwo)
+        val emojiChipThree = findViewById<EmojiChip>(R.id.emojiDialogChipThree)
+
+        GlobalScope.launch {
+            val emojiOne =
+                withContext(dispatchers.io) { repository.getByNameOrNull("grinning face with smiling eyes") }
+            val emojiTwo =
+                withContext(dispatchers.io) { repository.getByNameOrNull("rolling on the floor laughing") }
+            val emojiThree =
+                withContext(dispatchers.io) { repository.getByNameOrNull("face with tears of joy") }
+
+            withContext(dispatchers.main) {
+                emojiOne?.let { emojiChipOne.setEmoji(emojiViewModel = it.toViewModel()) }
+                emojiTwo?.let {
+                    emojiChipTwo.setEmoji(
+                        emojiViewModel = it.toViewModel(),
+                        count = 2
+                    )
+                }
+                emojiThree?.let {
+                    emojiChipThree.setEmoji(
+                        emojiViewModel = it.toViewModel(),
+                        count = 3
+                    )
+                }
+            }
+        }
     }
 }
