5b52c73e538a89f7fcacf92302d8b0fbc350f4b6
Christopher Keenan <ckeenan@starry.com>
2022-01-01 17:25:36 -0600

    Updated most of the build files to use kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.5.30")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.32")
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.0.0")
-        classpath("com.squareup.sqldelight:gradle-plugin:1.4.3")
+        classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
     }
 }
 
@@ -25,7 +25,6 @@ apply(plugin = "org.jetbrains.dokka")
 allprojects {
     repositories {
         google()
-        jcenter()
         mavenCentral()
         maven { url = uri("https://repo.repsy.io/mvn/chrynan/public") }
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
---
+++ emoji-repo-sqlite/build.gradle.kts
@@ -0,0 +1,98 @@
+import com.chrynan.emoji.buildSrc.LibraryConstants
+import com.chrynan.emoji.buildSrc.plugin.SqliteGeneratorPlugin
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("multiplatform")
+    id("kotlinx-serialization")
+    id("com.android.library")
+    id("maven-publish")
+    id("org.jetbrains.dokka")
+    id("com.squareup.sqldelight")
+}
+
+apply<SqliteGeneratorPlugin>()
+
+group = LibraryConstants.group
+version = LibraryConstants.versionName
+
+kotlin {
+    android {
+        publishAllLibraryVariants()
+        publishLibraryVariantsGroupedByFlavor = true
+    }
+    targets {
+        jvm()
+        ios()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+
+                api(project(":emoji-core"))
+            }
+        }
+    }
+}
+
+sqldelight {
+    database("EmojiDatabase") {
+        packageName = "com.chrynan.emoji.repo.sqlite"
+    }
+}
+
+android {
+    compileSdk = LibraryConstants.Android.compileSdkVersion
+
+    defaultConfig {
+        minSdk = LibraryConstants.Android.minSdkVersion
+        targetSdk = LibraryConstants.Android.targetSdkVersion
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            // Opt-in to experimental compose APIs
+            freeCompilerArgs = listOf(
+                "-Xopt-in=kotlin.RequiresOptIn"
+            )
+        }
+    }
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].java.srcDirs("src/androidMain/kotlin")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+
+    sourceSets["test"].java.srcDirs("src/androidTest/kotlin")
+    sourceSets["test"].res.srcDirs("src/androidTest/res")
+}
+
+tasks.withType<Jar> { duplicatesStrategy = DuplicatesStrategy.INHERIT }
+
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                url = uri("https://repo.repsy.io/mvn/chrynan/public")
+
+                credentials {
+                    username = (project.findProperty("repsyUsername") ?: System.getenv("repsyUsername")) as? String
+                    password = (project.findProperty("repsyToken") ?: System.getenv("repsyToken")) as? String
+                }
+            }
+        }
+    }
+}
