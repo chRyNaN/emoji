99681f1f52158df608d28c75d67f610717c8d4ce
Christopher Keenan <ckeenan@starry.com>
2022-01-01 18:47:18 -0600

    Removed Emojis object

--- emoji-core/src/commonMain/kotlin/com.chrynan.emoji.core/Emojis.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("unused")
-
-package com.chrynan.emoji.core
-
-/**
- * A Kotlin object useful for adding extension functions related to accessing a collection of
- * [Emoji]s.
- */
-object Emojis
